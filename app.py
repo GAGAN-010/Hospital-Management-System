a1d4249096659f2742fe79e82c16f3dc1b06a2d9
GAGAN-010 <ggit08751@gmail.com>
2026-01-15 12:02:45 -0500

    patient and preception

--- app.py
+++ app.py
@@ -12,12 +12,22 @@ class Hospital:
         self.root.title("Hospital Management System")
         self.root.geometry("1540x800+0+0")
 
-        lbltitle=Label(self.root,bd=20,relief=RIDGE,text="HOTEL MANAGEMENT SYSTEM",fg="red",bg="white",font=("times new roman",50,"bold"))
+        lbltitle=Label(self.root,bd=20,relief=RIDGE,text="HOSPITAL MANAGEMENT SYSTEM",fg="red",bg="white",font=("times new roman",50,"bold"))
         lbltitle.pack(side=TOP, fill=X)
 
         # ==========Data frame ========
         Dataframe=Frame(self.root,bd=20,relief=RIDGE)
         Dataframe.place(x=0,y=130,width=1530,height=480)
+
+        DataframeLeft=LabelFrame(Dataframe,bd=10,relief=RIDGE,font=("arial",12,"bold"),text="Patient Information")
+        DataframeLeft.place(x=0,y=5,width=980,height=350)
+
+        DataframeRight=LabelFrame(Dataframe,bd=10,relief=RIDGE,font=("arial",12,"bold"),text="Prescrption")
+        DataframeRight.place(x=990,y=5,width=460,height=350)
+
+
+
+
 
 root=Tk()
 ob=Hospital(root)
