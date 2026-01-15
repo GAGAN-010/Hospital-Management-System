f396b7b907adcac92ac65a82ed3a48dddf246218
GAGAN-010 <ggit08751@gmail.com>
2026-01-14 23:55:57 -0500

    border1

--- app.py
+++ app.py
@@ -12,7 +12,12 @@ class Hospital:
         self.root.title("Hospital Management System")
         self.root.geometry("1540x800+0+0")
 
+        lbltitle=Label(self.root,bd=20,relief=RIDGE,text="HOTEL MANAGEMENT SYSTEM",fg="red",bg="white",font=("times new roman",50,"bold"))
+        lbltitle.pack(side=TOP, fill=X)
 
+        # ==========Data frame ========
+        Dataframe=Frame(self.root,bd=20,relief=RIDGE)
+        Dataframe.place(x=0,y=130,width=1530,height=480)
 
 root=Tk()
 ob=Hospital(root)
